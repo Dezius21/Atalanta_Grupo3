800349b5a3a2cf7937458c0b3f6b07bb724a4ee6
RenkoDG <ediazg@student.ifp.es>
2026-03-17 13:45:20 +0100

    Datos controlador contacto

---
+++ backend/src/controllers/contactController.js
@@ -0,0 +1,29 @@
+const Contacto=require('../models/contact')
+
+exports.enviarFormulario = async (req,res) =>{
+    try{
+        const{tipoForm} = req.body;//Identificador del tipo de formulario
+        let insertId;
+
+        switch(tipoForm){
+            case 'general':
+                insertId = await Contacto.crearGeneral(req.body);
+                break;
+            case 'servicio':
+                insertId = await Contacto.crearServicio(req.body);
+                break;
+            case 'formacion':
+                insertId = await Contacto.crearServicio(req.body);
+                break;
+            default:
+                return res.status(400).json({error: 'Tipo de formulario no valido'});
+    }
+    res.status(201).json({
+        mensaje: 'Formulario enviado con exito',
+        id: insertId
+    });
+    } catch(error){
+        console.error("Error en contacto:",error);
+        res.status(500).json({error: 'Error interno del servidor al procesar el formulario'});
+    }
+};

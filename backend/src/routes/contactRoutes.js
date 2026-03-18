d9a1593daf3733e60ab7ec5bb262a15e16204aae
RenkoDG <ediazg@student.ifp.es>
2026-03-18 12:15:37 +0100

    Actualizacion rutas para integrar validacion de formulario

--- backend/src/routes/contactRoutes.js
+++ backend/src/routes/contactRoutes.js
@@ -1,8 +1,9 @@
 const express = require ('express');
 const router = express.Router();
 const contactController = require('../controllers/contactController');
+const {validarContacto}=require('../middlewares/validaciones');
 
 //Ruta POST para recibir cualquier tipo de formulario de contacto
-router.post('/enviar', contactController.enviarFormulario);
+router.post('/enviar', validarContacto,contactController.enviarFormulario);
 
 module.exports = router;

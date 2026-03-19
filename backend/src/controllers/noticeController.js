53e48661669a7ec08d36ac48f38542fc0acb1a67
RenkoDG <ediazg@student.ifp.es>
2026-03-19 10:10:01 +0100

    Controlador de noticias actualizados

--- backend/src/controllers/noticeController.js
+++ backend/src/controllers/noticeController.js
@@ -36,15 +36,21 @@ const crearNoticia = async (req,res) => {
         const {titulo, contenido, categoria} = req.body;
         
         const slug = titulo.toLowerCase()
+            .normalize('NFD')
+            .replace(/[\u0300-\u036f]/g,'')
             .replace(/\s+/g, '-')       
-            .replace(/[^\w-]/g, '');    
+            .replace(/[^\w-]/g, '');
+                
 
         const [result] = await pool.execute(
-            'INSERT INTO noticias (titulo, slug, contenido, imagen_url, categoria, autor_id) VALUES (?,?,?,?,?,?)',
-            [titulo, slug, contenido, imagen_url, categoria, req.usuario.id]
+            'INSERT INTO noticias (titulo, slug, contenido, imagen_url, categoria, publicado, autor_id) VALUES (?,?,?,?,?,?,?)',
+            [titulo, slug, contenido, imagen_url, categoria, true, req.usuario.id]
         );
         res.status(201).json({mensaje: 'Noticia creada correctamente', id: result.insertId});
     }catch(error){
+        if(error.code === 'ER_DUP_ENTRY'){
+            return res.status(409).json({ error: 'Ya existe una noticia con un titulo similar. Usa un titulo diferente'});
+        }
         console.error('Error al crear noticia', error.message);
         res.status(500).json({error: 'Error interno del servidor'});
     }
